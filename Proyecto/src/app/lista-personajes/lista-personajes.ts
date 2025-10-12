3920e77703a436a09e8b898a481d7ac6217d7d97
markmr080 <markmr080@gmail.com>
2025-10-12 15:39:11 +0200

    a

--- Proyecto/src/app/lista-personajes/lista-personajes.ts
+++ Proyecto/src/app/lista-personajes/lista-personajes.ts
@@ -11,15 +11,16 @@ import { Personaje } from '../models/Personaje.model';
 export class ListaPersonajes implements OnInit{
   ngOnInit(): void {
     throw new Error('Method not implemented.');
+    
   }
-
-  personajes:Personaje[]=[
+   personajes:Personaje[]=[
     { nombre: 'Aragorn', raza: 'Humano', poder: 4, imagen: 'https://sm.ign.com/t/ign_latam/screenshot/default/aragorn11_rmuk.1280.jpg' },
     { nombre: 'Legolas', raza: 'Elfo', poder: 5, imagen: 'https://tierramedia.net/wp-content/uploads/legolas-1-1024x688.jpg' },
     { nombre: 'Gimli', raza: 'Enano', poder: 3, imagen: 'images/gimlii.jpg' },
     { nombre: 'Gandalf', raza: 'Mago', poder: 5, imagen: 'images/gandalf.jpeg' },
     { nombre: 'Thrall', raza: 'Orco', poder: 4, imagen: 'images/thrall.jpg' }
   ];
+ 
 
   
 }
